37f89d338351a0bdf380e64b5f9839073eeeb22e
Emile Cyimena <cyimena09@hotmail.com>
2021-03-11 21:04:17 +0100

    Initial commit

---
+++ src/main/java/be/cyimena/airbnbmessengerservice/AirbnbMessengerServiceApplication.java
@@ -0,0 +1,13 @@
+package be.cyimena.airbnbmessengerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbMessengerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbMessengerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/cyimena/airbnbmessengerservice/AirbnbMessengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cyimena.airbnbmessengerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbMessengerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
